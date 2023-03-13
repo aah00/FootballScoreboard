f1f073c749a5c4c2a18ab47af9ce97e9c56b5fc5
Abdullah Al Hasib <abdullah.alhasib@yahoo.com>
2023-03-13 17:01:53 +0100

    Created ScoreBoard interface

--- football_scoreboard_lib/include/ScoreBoard.hpp
+++ football_scoreboard_lib/include/ScoreBoard.hpp
@@ -1,3 +1,8 @@
+#pragma once
+
+#include "Result.hpp"
+#include <string>
+#include <vector>
 
 namespace LiveScore
 {
@@ -10,8 +15,67 @@ namespace LiveScore
 class ScoreBoard
 {
 public:
+
+    /**
+     * Constructor
+     */
     ScoreBoard();
+
+    /**
+     * Destructor
+     */
     ~ScoreBoard();
+
+    /**
+    * Start a game with the given team names.
+    *
+    * @param home_team Name of the home team.
+    * @param away_team Name of the away team.
+    * @return Result::Ok when the operation is successful or
+              Result::Game_already_exist if the game already exists.
+    */
+    Result start_game(std::string home_team, std::string away_team);
+
+    /**
+    * Update the score of an ongoing game.
+    *
+    * @param home_team Name of the home team.
+    * @param away_team Name of the away team.
+    * @param home_score Abosulute score of the home team.
+    * @param away_score Absolute score of the away team.
+    
+    * @return Result::Ok when the operation is successful or
+              Result::Game_not_found if the game does not exist.
+    */
+    Result set_score (std::string home_team, std::string away_team,
+            unsigned int home, unsigned int away);
+
+    /**
+    * End a gave given by two team-names.
+    *
+    * @param home_team Name of the home team.
+    * @param away_team Name of the away team.
+    
+    * @return Result::Ok when the operation is successful or
+              Result::Game_not_found if the game does not exist.
+    */
+    Result end_game (std::string home_team, std::string away_team);
+
+    /**
+    * Get the summary of ongoing games.
+
+    * @return summary of ongoing games in sorted order.
+    */
+    std::string get_summary() const;
+
+
+    /**
+    * Get the summary of ongoing games.
+    * @param summary empty vector to be updated by match results.
+    * 
+    * @return Result::Ok when the operation is successful
+    */
+    Result get_summary(std::vector<std::string>& summary) const;
 };
 
 }
--- football_scoreboard_lib/src/ScoreBoard.cpp
+++ football_scoreboard_lib/src/ScoreBoard.cpp
@@ -10,3 +10,68 @@ ScoreBoard::ScoreBoard()
 // Destructor
 ScoreBoard::~ScoreBoard()
 {}
+
+/**
+* Start a game with the given team names.
+*
+* @param home_team Name of the home team.
+* @param away_team Name of the away team.
+* @return Result::Ok when the operation is successful or
+          Result::Game_already_exist if the game already exists.
+*/
+Result ScoreBoard::start_game(std::string home_team, std::string away_team)
+{
+    throw "Function not implemented.";
+}
+
+/**
+* Update the score of an ongoing game.
+*
+* @param home_team Name of the home team.
+* @param away_team Name of the away team.
+* @param home_score Abosulute score of the home team.
+* @param away_score Absolute score of the away team.
+
+* @return Result::Ok when the operation is successful or
+          Result::Game_not_found if the game does not exist.
+*/
+Result ScoreBoard::set_score (std::string home_team, std::string away_team,
+        unsigned int home, unsigned int away)
+{
+    throw "Function not implemented.";
+}
+
+/**
+* End a gave given by two team-names.
+*
+* @param home_team Name of the home team.
+* @param away_team Name of the away team.
+
+* @return Result::Ok when the operation is successful or
+          Result::Game_not_found if the game does not exist.
+*/
+Result ScoreBoard::end_game (std::string home_team, std::string away_team)
+{
+    throw "Function not implemented.";
+}
+
+/**
+* Get the summary of ongoing games.
+
+* @return summary of ongoing games in sorted order.
+*/
+std::string ScoreBoard::get_summary() const
+{
+    throw "Function not implemented.";
+}
+
+/**
+* Get the summary of ongoing games.
+* @param ScoreBoard::summary empty vector to be updated by match results.
+* 
+* @return Result::Ok when the operation is successful
+*/
+Result ScoreBoard::get_summary(std::vector<std::string>& summary) const
+{
+    throw "Function not implemented.";
+}
